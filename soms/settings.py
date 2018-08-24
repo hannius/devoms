34db5885a16a15626f65a6f7ef408776275e46b2
leiyawu <leiyawu@netmarble.com>
2018-08-24 17:53:54 +0800

    for add test

--- soms/settings.py
+++ soms/settings.py
@@ -88,6 +88,7 @@ WSGI_APPLICATION = 'soms.wsgi.application'
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#for diy mysql config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
